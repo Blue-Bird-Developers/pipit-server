f23e2507356af1eafddc3c48728482e08267b9f3
juhyeonme <kjh96905@gmail.com>
2021-01-07 15:57:29 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/bludbird/pipit/PipitApplication.java
@@ -0,0 +1,13 @@
+package com.bludbird.pipit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bludbird/pipit/PipitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bludbird.pipit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
