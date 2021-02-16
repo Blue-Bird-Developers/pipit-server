ed10bb512930ad7c6b7f6d5c1c9564a541753f7d
Ellie.sin <ellie.sin@kakaoenterprise.com>
2021-02-16 21:34:40 +0900

    회원가입, 로그인을 위한 Spring Security configuration, filter, userDetails 정의

--- authentication-api/src/main/java/com/bluebird/pipit/AuthenticationApiApplication.java
+++ authentication-api/src/main/java/com/bluebird/pipit/AuthenticationApiApplication.java
@@ -1,5 +1,6 @@
 package com.bluebird.pipit;
 
+import com.bluebird.pipit.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +9,7 @@ import com.bluebird.pipit.portal.ConnectionProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ConnectionProperties.class)
+@EnableConfigurationProperties(AppProperties.class)
 public class AuthenticationApiApplication {
 
 	public static void main(String[] args) {
---
+++ authentication-api/src/main/java/com/bluebird/pipit/domain/user/domain/AuthProvider.java
@@ -0,0 +1,6 @@
+package com.bluebird.pipit.domain.user.domain;
+
+public enum AuthProvider {
+    local,
+    github
+}
