54828804c39eff3a897f86bc1c8b25810b2a4a00
juhyeonme <kjh96905@gmail.com>
2021-03-11 19:19:32 +0900

    [#12] 공통 exception 수정 및 불필요한 요소 삭제

--- support/src/main/java/com/bluebird/pipit/support/exception/GeneralExceptionCode.java
+++ support/src/main/java/com/bluebird/pipit/support/exception/GeneralExceptionCode.java
@@ -3,7 +3,5 @@ package com.bluebird.pipit.support.exception;
 public enum GeneralExceptionCode implements ExceptionCode {
 	GENERAL_ERROR,
 	INVALID_PARAMETER,
-	EXTERNAL_SERVER_ERROR,
-	SERVICE_INVALID,
-	RULE_VIOLATION
+	INVALID_BUSINESS
 }
